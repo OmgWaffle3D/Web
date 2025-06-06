9da28ce86507675bd6bf7a7c9e80d0601957d42e
OmgWaffle3D <A01198876@tec.mx>
2025-06-05 20:16:38 -0600

    Práctica Web + BD + VJ

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -1,14 +1,14 @@
 window.onload = () => {
     if (sessionStorage.id) {
         const link = document.getElementById("linkGame");
-        link.setAttribute("href", "./pages/game.html?id=" + sessionStorage.id);    
+        link.setAttribute("href", "./pages/index.html?id=" + sessionStorage.id);    
     }else {
         window.location = "./index.html";
     }
 };
 
 const link = document.getElementById("linkGame");
-link.setAttribute("href", "./pages/game.html?id=" + sessionStorage.id);
+link.setAttribute("href", "./pages/index.html?id=" + sessionStorage.id);
 
 const logout = document.getElementById("logout");
 logout.addEventListener("click", () => {
